b9be274b18b2d90ff17d28d9c40371714e63a225
D4VOS <dawid.bryla@student.put.poznan.pl>
2021-07-27 21:52:01 +0200

    Added Models

--- edziekanat/admin.py
+++ edziekanat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from edziekanat.models import *
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Employee)
+admin.site.register(Institute)
+admin.site.register(Subject)
--- edziekanat/models.py
+++ edziekanat/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Institute(models.Model):
+    name = models.CharField(max_length=70, unique=True)
+
+
+class User(models.Model):
+    email = models.CharField(max_length=70, unique=True)
+    password = models.EmailField()
+
+    class Meta:
+        abstract = True
+
+
+class Employee(User):
+    role = models.CharField(max_length=10)
+    institute = models.ForeignKey(Institute, on_delete=models.CASCADE)
+
+
+class Student(User):
+    indexNumber = models.IntegerField(unique=True)
+
+
+class Subject(models.Model):
+    institution = models.ForeignKey(to=Institute, on_delete=models.CASCADE)
+    name = models.CharField(max_length=70, unique=True)
+    sem = models.IntegerField()
