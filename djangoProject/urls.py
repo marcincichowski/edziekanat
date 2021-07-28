7bd9f3e8296eb2aeb9c7f83f1dadca4291142b7e
D4VOS <dawid.bryla@student.put.poznan.pl>
2021-07-28 22:11:33 +0200

    eDziekanat url added

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+import edziekanat
 from edziekanat import urls
 
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('edziekanat/', include(edziekanat.urls)),
     path('admin/', admin.site.urls),
 ]
 
