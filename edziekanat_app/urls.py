95bc87e75b4564173720decdce818617a9f19b42
MarcinCichowski <marcin.cichowski@student.put.poznan.pl>
2021-09-14 19:35:08 +0200

    Add subpages, refactor index page.

--- edziekanat_app/urls.py
+++ edziekanat_app/urls.py
@@ -6,5 +6,9 @@ app_name = 'edziekanat_app'
 urlpatterns = [
     url(r'^$', views.index, name="home"),
     url(r'^login/$', views.user_login, name='user_login'),
-    url(r'^register/$', views.user_register, name='user_register')
+    url(r'^register/$', views.user_register, name='user_register'),
+    url(r'^applications/$', views.applications, name='applications'),
+    url(r'^administrators/$', views.administrators, name='administrators'),
+    url(r'^settings/$', views.settings, name='settings'),
+    url(r'^dictionaries/$', views.dictionaries, name='dictionaries')
 ]
