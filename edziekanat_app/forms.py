9fe81d8dc9e11ec58fd63fafc52bdba8bff5be7c
MarcinCichowski <marcin.cichowski@student.put.poznan.pl>
2021-09-14 22:57:28 +0200

    add Tabs and form to dictionary page

--- edziekanat_app/forms.py
+++ edziekanat_app/forms.py
@@ -12,3 +12,11 @@ class RegisterForm(forms.Form):
 class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'input is-medium', 'placeholder': 'Adres e-mail'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium', 'placeholder': 'Hasło'}))
+
+
+class addDictionaryValueCathedral(forms.Form):
+    value = forms.CharField(widget=forms.TextInput(attrs={'class': 'input is-primary is-fullwidth', 'placeholder': 'Nazwa katedry'}))
+
+    def __init__(self, *args, **kwargs):
+        super(addDictionaryValueCathedral, self).__init__(*args, **kwargs)
+        self.fields['value'].label = ""
