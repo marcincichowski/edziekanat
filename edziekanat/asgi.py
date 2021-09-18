85bf486c95648cba059c97f28fa024c1c13ab521
D4VOS <dawid.bryla@student.put.poznan.pl>
2021-09-18 13:10:05 +0200

    fixed module eDziekanat not found

--- edziekanat/asgi.py
+++ edziekanat/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for eDziekanat project.
+ASGI config for edziekanat project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eDziekanat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edziekanat.settings')
 
 application = get_asgi_application()
