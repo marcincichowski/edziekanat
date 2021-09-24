8e4a823882447b5d9d4a79e0660f41b243be9941
stim4pl <kacper.jankowski9@gmail.com>
2021-09-24 22:58:48 +0200

    Fix

--- edziekanat_app/models/tables/course.py
+++ edziekanat_app/models/tables/course.py
@@ -8,10 +8,10 @@ class Course(Model):
                      unique=True,
                      verbose_name=_('Nazwa'))
 
-    department = ForeignKey(to='edziekanat_app.Department',
-                            verbose_name=_('Instytut'),
-                            on_delete=CASCADE,
-                            related_name="departments", default=None, null=True)
+    faculty = ForeignKey(to='edziekanat_app.Faculty',
+                         verbose_name=_('Wydział'),
+                         on_delete=CASCADE,
+                         related_name="facultiess", default=None, null=True)
 
     degree = IntegerField(verbose_name=_('Stopień'),
                           default=1,
